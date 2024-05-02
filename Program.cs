313cec440fd6ba5e2215e57d0793c7891fa762eb
Itoshi Rin <86209431+It0shiR1n@users.noreply.github.com>
2024-05-02 00:14:24 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Program {
+    public class AutomateTask{
+        public static void Main(string[] args) {
+            
+            int i = 0;          // How many images you will download ?
+            string xPath = "";  // Set The xPath
+            string url = "";
+            
+            ChromeOptions options = new ChromeOptions();
+            options.BinaryLocation = @"/usr/bin/chromium";   // Binary Chromium Location
+            options.AddArgument("--headless");               // Withou window
+
+            IWebDriver driver = new ChromeDriver(options);
+
+            for (; i <= 2;i++){
+                driver.Navigate().GoToUrl(url);
+
+                IWebElement imageElement = driver.FindElement(By.XPath(xPath));
+                WebClient webClient = new WebClient();
+
+                string imageURL = imageElement.GetAttribute("src");
+
+                byte[] image = webClient.DownloadData(imageURL);
+                File.WriteAllBytes($"img{i}.jpg", image);
+
+            }
+        
+            driver.Quit();
+
+        }
+    }
+}
